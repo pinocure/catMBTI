cd8476b9b3cb694c78dc21dd45f169df5398965d
pinocure <xodiq@naver.com>
2025-12-14 00:37:42 +0900

    Page Route

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,31 @@ import React from "react";
 import styled from 'styled-components';
 import PangImage from '../assets/cat1.jpg';
 import Button from 'react-bootstrap/Button';
+import {useNavigate} from 'react-router-dom';
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const handleClickButton = () => {
+        navigate('/question');
+    }
 
     return (
         <Wrapper>
             <Header>예비집사 판별기</Header>
+
             <Contents>
                 <Title>나에게 맞는 주인님은?</Title>
                 <LogoImage>
                     <img src={PangImage} className="rounded-circle" width={350} height={350} />
                 </LogoImage>
                 <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
-                <Button>테스트 시작하기</Button>
+                <Button style={{fontFamily: "Pretendard-Regular"}} onClick={handleClickButton}>
+                    테스트 시작하기
+                </Button>
             </Contents>
+
         </Wrapper>
     )
     
@@ -33,6 +44,7 @@ const Header = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    font-family: "Pretendard-Regular";
 `
 
 const Contents = styled.div`
@@ -45,6 +57,7 @@ const Contents = styled.div`
 const Title = styled.div`
     font-size: 30pt;
     margin-top: 40px;
+    font-family: "Pretendard-Regular";
 `
 
 const LogoImage = styled.div`
@@ -54,4 +67,5 @@ const LogoImage = styled.div`
 const Desc = styled.div`
     font-size: 20pt;
     margin-top: 20px;
+    font-family: "Pretendard-Regular";
 `
